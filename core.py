50daa05fea80eead283e17546ed856180d69fe14
ivan1994petrov <ivan94hristov@gmail.com>
2020-04-11 03:36:10 +0300

    use threading for speed

--- core.py
+++ core.py
@@ -10,31 +10,34 @@ TEST_SITES = [
 ]
 
 proxy_list = get_proxy_list()
+active_proxy_list = []
+
 
 def check_proxy(proxy, protocol):
     url = random.choice(TEST_SITES)
     try:
         r = requests.head(url, proxies={protocol: proxy})
-        print(f'r.status_code   {r.status_code}')
         if r.status_code == 200:
-            return True
+            active_proxy_list.append([proxy, protocol])
         return
     except:
         return
 
-active_proxy_list = []
-counter = 0
-for item in proxy_list:
-    ip = item[0]
-    port = item[1]
-    https = item[3]
+def get_free_https_proxies():
 
-    if https == 'yes' or 'Yes':
-        counter +=1
-        if check_proxy(f'{ip}:{port}', 'https'):
-            active_proxy_list.append(item)
+    threads = []
 
+    for item in proxy_list:
+        ip = item[0]
+        port = item[1]
+        https = item[3]
 
+        if https == 'yes' or https == 'Yes':
+            t = threading.Thread(target=check_proxy, args=[f'{ip}:{port}', 'https'])
+            t.start()
+            threads.append(t)
 
-print(len(proxy_list))
-print(counter)
+    for thread in threads:
+        thread.join(timeout=5)
+
+get_free_https_proxies()
